a72bd111a8e560200ebf9f785d3760c02e42b7cc
Richard Jones <richard.jones@cloudtechnologyoffice.com>
2024-01-19 12:32:42 +0000

    Update VPC capabilities

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  vpc_id     = sort(data.aws_vpcs.selected.ids)[0]
+  vpc_id     = sort(data.aws_vpcs.main.ids)[0]
   region     = data.aws_region.current.name
   subnet_ids = data.aws_security_groups.all.ids
   account_id = data.aws_caller_identity.current.id
